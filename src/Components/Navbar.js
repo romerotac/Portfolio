7413da29405fff78a5f6023eb1f7190cb6665a84
romerotac <christian.romero02.cr@gmail.com>
2022-08-09 17:11:18 -0400

    small changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,9 +7,7 @@ function Navbar() {
         <>
         <div className='menu'>
             <div className='nav'>
-            <div className='logo-position'>   
-            <img src={require('../Images/logo.png')}></img>
-            </div> 
+
             <ul>
                 <li><Link to = "/"> Home </Link></li>
                 <li><Link to = "/about">About</Link></li>
