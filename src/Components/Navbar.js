c759e3f3d7a26a7a9b074304474435619d1d3730
romerotac <christian.romero02.cr@gmail.com>
2022-08-10 11:24:16 -0400

    App.css modified

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,9 @@ function Navbar() {
         <>
         <div className='menu'>
             <div className='nav'>
-
+            <div className='logo-position'>   
+            <img src={require('../Images/logo.png')}></img>
+            </div> 
             <ul>
                 <li><Link to = "/"> Home </Link></li>
                 <li><Link to = "/about">About</Link></li>
