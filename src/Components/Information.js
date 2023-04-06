c7a603bb88c09389e8c454394ee2a09a42b55471
CRTaipe <christian.romero02.cr@gmail.com>
2023-04-06 08:21:10 +0200

    Added new projects in portfolio

--- src/Components/Information.js
+++ src/Components/Information.js
@@ -1,6 +1,6 @@
 import {AiFillHtml5} from 'react-icons/ai';
 import {FaCss3Alt,FaReact} from 'react-icons/fa';
-
+import {SiJavascript} from 'react-icons/si'
 
 /**Project 1 URL */
 let project1ImageCover = require('../Images/project1/1.png');
@@ -8,16 +8,27 @@ let project1Image2 = require('../Images/project1/2.png');
 let project1Image3 = require('../Images/project1/3.png');
 let project1Image4 = require('../Images/project1/4.png');
 
+/**Project 2 URL */
+let project2ImageCover = require('../Images/project2/FunApp1.PNG');
+let project2Image2 = require('../Images/project2/FunApp4.png');
+let project2Image3 = require('../Images/project2/FunApp2.PNG');
+let project2Image4 = require('../Images/project2/FunApp3.PNG');
 
-let coverPath = require('../Images/empty_project.jpg')
-let assestPath = require('../Images/empty_project.jpg')
+/**Project 4 URL */
+let project4ImageCover = require('../Images/project4/0.PNG');
+let project4Image2 = require('../Images/project4/1.png');
+
+
+/** No project space */
+let coverPath = require('../Images/empty_project.jpg');
+let assestPath = require('../Images/empty_project.jpg');
 
 const Information = [{
     id:1,
     title: 'Personal Portfolio',
     description: 'The Project was made by using ReactJs framework and css.',
     cover:project1ImageCover,
-    demoURL:'https://christian-romero-taipe.herokuapp.com',
+    demoURL:'https://christianromerotaipe.netlify.app/',
     codeURL:'https://github.com/romerotac/Portfolio',
     icons:[{
         id:1,
@@ -57,11 +68,11 @@ const Information = [{
 
 {
     id:2,
-    title: 'Project Number 2',
-    description: 'This is the description of the second project',
-    cover:coverPath,
-    demoURL:'null',
-    codeURL:'null',
+    title: 'FunWeb',
+    description: 'Project about creating a two fun pages, one containing a randomizer music player with the usage of Spotify API, and another a random generator of Chuck Norris Jokes',
+    cover:project2ImageCover,
+    demoURL:'https://myfunweb.netlify.app/',
+    codeURL:'https://github.com/romerotac/FunWebDeployment',
     icons:[{
         id:1,
         icon:<AiFillHtml5/>
@@ -77,18 +88,23 @@ const Information = [{
     ],
     images:[{
         id:1,
-        src: assestPath,
+        src: project2ImageCover,
         alt: "Image1"
         },
         {
         id:2,
-        src: assestPath,
+        src: project2Image2,
         alt: "Image2"
         },
         {
         id:3,
-        src: assestPath,
+        src: project2Image3,
         alt: "Image3"
+        },
+        {
+        id:4,
+        src: project2Image4,
+        alt: "Image4"
         }
     ], 
 },
@@ -131,11 +147,11 @@ const Information = [{
 },
 {
     id:4,
-    title: 'Project Number 4',
-    description: 'This is the description of the fourth project',
-    cover:coverPath,
-    demoURL:'null',
-    codeURL:'null',
+    title: 'Expense Tracker',
+    description: 'Calculator of expeses made in JavaScript',
+    cover:project4ImageCover,
+    demoURL:'https://romerotac.github.io/JS-basic/',
+    codeURL:'https://github.com/romerotac/JS-basic',
     icons:[{
         id:1,
         icon:<AiFillHtml5/>
@@ -146,23 +162,18 @@ const Information = [{
         },
         {
         id:3,
-        icon:<FaReact/>
+        icon:<SiJavascript/>
         },
     ],
     images:[{
         id:1,
-        src: assestPath,
+        src: project4ImageCover,
         alt: "Image1"
         },
         {
         id:2,
-        src: assestPath,
+        src: project4Image2,
         alt: "Image2"
-        },
-        {
-        id:3,
-        src: assestPath,
-        alt: "Image3"
         }
     ], 
 },
